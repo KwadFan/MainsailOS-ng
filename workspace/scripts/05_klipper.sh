20b7812dc437428a97c29a221c04db1d9d8a7080
Stephan Wendel <me@stephanwe.de>
2022-04-23 12:57:29 +0200

    refactor: refactored 05_klipper.sh
    Moved setup variables to config.local
    
    Added EDITBASE_KLIPPER_IS_INSTALL to make Input shaper Pre Install
    configureable, uses bool: 1: yes, 0:no
    
    Enables also SPI in config.txt (if disabled)
    
    Signed-off-by: Stephan Wendel <me@stephanwe.de>

--- workspace/scripts/05_klipper.sh
+++ workspace/scripts/05_klipper.sh
@@ -18,60 +18,44 @@ export LC_ALL=C
 source /common.sh
 install_cleanup_trap
 
-# Setup Vars
-BASE_USER="pi"
-
-KLIPPER_SRC_DIR="/home/${BASE_USER}/klipper"
-KLIPPER_PYTHON_DIR="/home/${BASE_USER}/klippy-env"
-
-KLIPPER_REPO_SHIP="https://github.com/Klipper3d/klipper.git"
-KLIPPER_REPO_BRANCH="master"
-KLIPPER_DEPS=(git virtualenv python3-dev libffi-dev build-essential \
-libncurses-dev libusb-dev avrdude gcc-avr binutils-avr avr-libc \
-stm32flash dfu-util libnewlib-arm-none-eabi \
-gcc-arm-none-eabi binutils-arm-none-eabi libusb-1.0-0)
-KLIPPER_USER_GROUPS="tty,dialout"
-KLIPPER_USER_DIRS="klipper_config klipper_logs gcode_files"
-KLIPPER_PYENV_REQ="scripts/klippy-requirements.txt"
-
+echo_green "Installing Klipper and enable klippy Service ..."
 # Install service file
 unpack /files/klipper/root /
 
-echo_green "Installing Klipper and enable klippy Service ..."
-
 ### install all deps at once for time consumption reasons.
 ## APT: Update Repo Database and install Dependencies
-
 apt update
-apt install --yes --no-install-recommends "${KLIPPER_DEPS[@]}"
+# shellcheck disable=SC2086
+# Disable SC2086 here, because we want word splitting
+apt install --yes --no-install-recommends ${EDITBASE_KLIPPER_DEPS}
 
-### Make sure user pi has access to serial ports
-## NOTE: BASE_USER is defined by CustomPIOS Variable
-## there for if you plan to set something else than "pi"
-## Currently CustomPIOS uses "pi"
-
-usermod -a -G "${KLIPPER_USER_GROUPS}" "${BASE_USER}"
+### Make sure first user (EDITBASE_BASE_USER) has access to serial ports
+usermod -a -G "${EDITBASE_KLIPPER_USER_GROUPS}" "${EDITBASE_BASE_USER}"
 
 ### clone klipper repo
-pushd /home/"${BASE_USER}" &> /dev/null || exit 1
-sudo -u "${BASE_USER}" git clone -b "${KLIPPER_REPO_BRANCH}" "${KLIPPER_REPO_SHIP}" klipper
+pushd /home/"${EDITBASE_BASE_USER}" &> /dev/null || exit 1
+sudo -u "${EDITBASE_BASE_USER}" \
+git clone -b "${EDITBASE_KLIPPER_REPO_BRANCH}" "${EDITBASE_KLIPPER_REPO_SHIP}" klipper
 ### Create needed dirs
-for dirs in ${KLIPPER_USER_DIRS}; do
-    if [ -d "/home/${BASE_USER}/${dirs}" ]; then
+for dirs in ${EDITBASE_KLIPPER_USER_DIRS}; do
+    if [ -d "/home/${EDITBASE_BASE_USER}/${dirs}" ]; then
         echo_green "${dirs} already exists!"
     else
         echo_green "Creating ${dirs}"
-        sudo -u "${BASE_USER}" mkdir -p "${dirs}"
+        sudo -u "${EDITBASE_BASE_USER}" mkdir -p "${dirs}"
     fi
 done
 popd &> /dev/null || exit 1
 
 # create python virtualenv and install klipper requirements
-pushd /home/"${BASE_USER}" &> /dev/null || exit 1
+pushd /home/"${EDITBASE_BASE_USER}" &> /dev/null || exit 1
 echo_green "Creating Virtualenv for Klipper (klipper-env) ..."
-sudo -u "${BASE_USER}" virtualenv -p python3 "${KLIPPER_PYTHON_DIR}"
+sudo -u "${EDITBASE_BASE_USER}" \
+virtualenv -p python3 "${EDITBASE_KLIPPER_PYTHON_DIR}"
 echo_green "Installing klippy Python Dependencies ..."
-sudo -u "${BASE_USER}" "${KLIPPER_PYTHON_DIR}"/bin/pip install -r "${KLIPPER_SRC_DIR}"/"${KLIPPER_PYENV_REQ}"
+sudo -u "${EDITBASE_BASE_USER}" \
+"${EDITBASE_KLIPPER_PYTHON_DIR}"/bin/pip install \
+-r "${EDITBASE_KLIPPER_SRC_DIR}"/"${EDITBASE_KLIPPER_PYENV_REQ}"
 popd &> /dev/null || exit 1
 
 
@@ -80,57 +64,38 @@ systemctl_if_exists enable klipper.service
 
 echo_green "... done!"
 
+
 ### Input Shaper preinstall
-
-# Setup Vars
-
-IS_REQ_PREINSTALL_DEPS=(python3-numpy python3-matplotlib)
-IS_REQ_PREINSTALL_PIP="numpy"
-IS_REQ_PREINSTALL_CFG_FILE="/boot/config.txt"
+if [ "${EDITBASE_KLIPPER_IS_INSTALL}" == "1" ]; then
+    echo_green "Perform Input Shaper preinstall ..."
 
 
-echo_green "Input Shaper Requirements preinstall"
+    # install all deps at once for time consumption reasons.
+    echo_green "Installing Input Shaper Dependencies ..."
+    apt update
+    # shellcheck disable=SC2086
+    # Disable SC2086 here, because we want word splitting
+    apt install --yes --no-install-recommends ${EDITBASE_IS_DEPS}
 
-### install all deps at once for time consumption reasons.
-## APT: Update Repo Database and install Dependencies
 
-echo_green "Installing Input Shaper Dependencies ..."
-apt update
-apt install --yes --no-install-recommends "${IS_REQ_PREINSTALL_DEPS[@]}"
-
-### numpy Parallelizing
-function max_cores() {
-    local core_count
-    core_count="$(nproc)"
-    if [ "${core_count}" -lt 3 ]; then
-        echo "${core_count}"
+    ### Check for Klipper Venv installed.
+    pushd /home/"${EDITBASE_BASE_USER}" &> /dev/null || exit 1
+    if [ -d "${EDITBASE_KLIPPER_PYTHON_DIR}" ] && \
+    [ -x "${EDITBASE_KLIPPER_PYTHON_DIR}/bin/pip" ]; then
+        echo_green "Installing numpy..."
+        sudo -u "${EDITBASE_BASE_USER}" \
+        "${EDITBASE_KLIPPER_PYTHON_DIR}"/bin/pip install -v "${EDITBASE_IS_PIP}"
     else
-        echo "$(("${core_count}" -1))"
+        echo_red "Klipper Venv not found! [EXITING]!"
+        exit 1
     fi
-}
+    ## Cleanup
+    sudo -u "${EDITBASE_BASE_USER}" rm -rf /home/"${EDITBASE_BASE_USER}"/.cache
+    popd &> /dev/null || exit 1
 
-echo_green "System has $(nproc) Cores available."
-echo_green "Using $(max_cores)..."
-NPY_NUM_BUILD_JOBS="$(max_cores)"
-export NPY_NUM_BUILD_JOBS
+    ## Enable spi interface by default
+    echo_green "Enabling SPI Interface..."
+    sed -i 's/#dtparam=spi=on/dtparam=spi=on/' "${EDITBASE_IS_CFG_FILE}"
 
-
-### Check for Klipper Venv installed.
-pushd /home/"${BASE_USER}" &> /dev/null || exit 1
-if [ -d "${KLIPPER_PYTHON_DIR}" ] && [ -x "${KLIPPER_PYTHON_DIR}/bin/pip" ]; then
-    echo_green "Installing numpy..."
-    sudo -u "${BASE_USER}" "${KLIPPER_PYTHON_DIR}"/bin/pip install -v "${IS_REQ_PREINSTALL_PIP}"
-else
-    echo_red "Klipper Venv not found!"
-    return 1
+    echo_green "... done!"
 fi
-
-## Cleanup
-sudo -u "${BASE_USER}" rm -rf /home/"${BASE_USER}"/.cache
-popd &> /dev/null || exit 1
-
-## Enable spi interface by default
-echo_green "Enabling SPI Interface..."
-sed -i 's/#dtparam=spi=on/dtparam=spi=on/' "${IS_REQ_PREINSTALL_CFG_FILE}"
-
-echo_green "... done!"
